5aca057f2de783dc15c62ebf0beb9da71c69a69e
Emmael TAYOT <mariskabiloghe@yahoo.fr>
2021-08-16 23:06:45 +0200

    formulaire valider terminer coté html

---
+++ src/app/pages/formulaire.js
@@ -0,0 +1,20 @@
+// let from = document.getElementById('form')
+
+// form.addEventListener('submit', function(e){
+//      let submit = document.getElementById('submit');
+//      if(submit.value.trin() == ""){
+//         let myerror = document.getElementById('myerror');
+//         myerror.innerHTML = "champ error est requis";
+//         myerror.style.color = "red";
+//          e.preventDefault();
+//      }
+
+
+// });
+
+// let nom = document.getElementById('nom');
+// let prenom = document.getElementById('prenom');
+// let adresse = document.getElementById('adresse');
+// let boitePostale = document.getElementById('boite-postale');
+// let ville = document.getElementById('ville');
+// let addresseMail = document.getElementById('adresse-mail');
