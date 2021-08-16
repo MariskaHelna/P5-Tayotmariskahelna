c1bfd13a4a0626dbc09cf7fb4187e32533bca6e1
Emmael TAYOT <mariskabiloghe@yahoo.fr>
2021-08-16 23:39:04 +0200

    formulaire validation coté html modifié

--- src/app/pages/formulaire.js
+++ src/app/pages/formulaire.js
@@ -1,20 +1,20 @@
-// let from = document.getElementById('form')
+let from = document.getElementById('form')
 
-// form.addEventListener('submit', function(e){
-//      let submit = document.getElementById('submit');
-//      if(submit.value.trin() == ""){
-//         let myerror = document.getElementById('myerror');
-//         myerror.innerHTML = "champ error est requis";
-//         myerror.style.color = "red";
-//          e.preventDefault();
-//      }
+form.addEventListener('submit', function(e){
+     let submit = document.getElementById('submit');
+     if(submit.value.trin() == ""){
+        let myerror = document.getElementById('myerror');
+        myerror.innerHTML = "champ error est requis";
+        myerror.style.color = "red";
+         e.preventDefault();
+     }
 
 
-// });
+});
 
-// let nom = document.getElementById('nom');
-// let prenom = document.getElementById('prenom');
-// let adresse = document.getElementById('adresse');
-// let boitePostale = document.getElementById('boite-postale');
-// let ville = document.getElementById('ville');
-// let addresseMail = document.getElementById('adresse-mail');
+
+// let firstName = document.getElementById('nom');
+// let lastName = document.getElementById('prenom');
+// let adress = document.getElementById('adresse');
+// let city = document.getElementById('ville');
+// let  = document.getElementById('adresse-mail');
